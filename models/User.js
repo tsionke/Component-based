9689b8097fb13503cd3158b1506c75b3a7e06e6a
tsionke <163241861+tsionke@users.noreply.github.com>
2026-04-09 11:27:51 +0300

    add modules

---
+++ models/User.js
@@ -0,0 +1,43 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database.js';
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  otp: {
+    type: DataTypes.STRING(6)
+  },
+  otp_expires: {
+    type: DataTypes.DATE
+  },
+  isVerified: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  role: {
+    type: DataTypes.ENUM('citizen', 'admin'),
+    defaultValue: 'citizen'
+  }
+}, {
+  tableName: 'Users',
+  timestamps: true
+});
+
+export default User;
+
